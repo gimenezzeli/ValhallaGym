f879244555dab97c92e84ea9646bf57b1717ac99
Elii <121507071+gimenezzeli@users.noreply.github.com>
2026-09-14 13:40:58 -0300

    Change error response for missing PASSWORD
    Updated error message for missing PASSWORD environment variable.

--- worker.js
+++ worker.js
@@ -266,10 +266,11 @@ export default {
   async fetch(request, env) {
 
     if (!env.PASSWORD) {
-  return new Response("NO_SECRET", { status: 500 });
-}
-
-return new Response("SECRET_OK", { status: 200 });
+  return new Response(
+    "Falta configurar el secret PASSWORD en Cloudflare.",
+    { status: 500 }
+  );
+    }
 
     const url = new URL(request.url);
 
